36837d76b5201799104b060d8094d943d11b657c
Diego Couto <die_go0107@yahoo.com.br>
2024-03-02 14:58:27 -0300

    first commit

---
+++ src/main/kotlin/com/coutech/financeirosystem/FinanceiroSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.coutech.financeirosystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceiroSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceiroSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/coutech/financeirosystem/FinanceiroSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coutech.financeirosystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceiroSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
