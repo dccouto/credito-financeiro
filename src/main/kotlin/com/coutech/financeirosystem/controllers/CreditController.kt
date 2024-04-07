ceb76dd04bd83a42c8ec33bee3494d7ff408adf3
dccouto <die_go0107@yahoo.com.br>
2024-04-07 18:07:22 -0300

    iniciando creditController

---
+++ src/main/kotlin/com/coutech/financeirosystem/controllers/CreditController.kt
@@ -0,0 +1,8 @@
+package com.coutech.financeirosystem.controllers
+
+import com.coutech.financeirosystem.service.impl.CreditService
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CreditController(creditService: CreditService) {
+}
