1d4b746a8f237234fac383db3e79406e67441a3a
dccouto <die_go0107@yahoo.com.br>
2024-04-08 19:34:56 -0300

    criando customerDto e save

--- src/main/kotlin/com/coutech/financeirosystem/controllers/CustomerController.kt
+++ src/main/kotlin/com/coutech/financeirosystem/controllers/CustomerController.kt
@@ -1,18 +1,27 @@
 package com.coutech.financeirosystem.controllers
 
+import com.coutech.financeirosystem.dtos.CustomerDto
 import com.coutech.financeirosystem.entities.Customer
 import com.coutech.financeirosystem.service.impl.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(name = "customer")
-class CustomerController(customerService: CustomerService) {
+class CustomerController(private val customerService: CustomerService) {
 
-    fun createCustomer(): ResponseEntity<Customer> {
+    @PostMapping
+    fun createCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<Customer> {
         TODO("Realizar o save")
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customerDto.toEntity()))
+    }
+
+    fun getCustomerById(id: Long) : ResponseEntity<CustomerDto>{
         return ResponseEntity.status(HttpStatus.OK).build()
+
     }
 }
