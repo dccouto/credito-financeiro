c4a4f8bae7ba544f347aa7d929d19d198214dae0
dccouto <die_go0107@yahoo.com.br>
2024-04-08 20:15:18 -0300

    criando endpoints get e delete

--- src/main/kotlin/com/coutech/financeirosystem/controllers/CustomerController.kt
+++ src/main/kotlin/com/coutech/financeirosystem/controllers/CustomerController.kt
@@ -1,10 +1,14 @@
 package com.coutech.financeirosystem.controllers
 
 import com.coutech.financeirosystem.dtos.CustomerDto
+import com.coutech.financeirosystem.dtos.CustomerView
 import com.coutech.financeirosystem.entities.Customer
 import com.coutech.financeirosystem.service.impl.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,12 +20,19 @@ class CustomerController(private val customerService: CustomerService) {
 
     @PostMapping
     fun createCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<Customer> {
-        TODO("Realizar o save")
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customerDto.toEntity()))
     }
 
-    fun getCustomerById(id: Long) : ResponseEntity<CustomerDto>{
-        return ResponseEntity.status(HttpStatus.OK).build()
+    @GetMapping("/{id}")
+    fun getCustomerById(@PathVariable(name = "id") id: Long) : ResponseEntity<CustomerView>{
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customerService.findById(id)))
+
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomerById(@PathVariable(name = "id") id: Long) : ResponseEntity<CustomerView>{
+        customerService.delete(id)
+        return ResponseEntity.noContent().build()
 
     }
 }
