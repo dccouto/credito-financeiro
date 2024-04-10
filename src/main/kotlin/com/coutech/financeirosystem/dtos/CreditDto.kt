287524e78086d0eb1b5ce975846a89a5dfacf09a
dccouto <die_go0107@yahoo.com.br>
2024-04-09 22:30:00 -0300

    criando creditDto

---
+++ src/main/kotlin/com/coutech/financeirosystem/dtos/CreditDto.kt
@@ -0,0 +1,22 @@
+package com.coutech.financeirosystem.dtos
+
+import com.coutech.financeirosystem.entities.Credit
+import com.coutech.financeirosystem.entities.Customer
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class CreditDto(
+    val creditValue: BigDecimal,
+    val dayFirstOfInstallment: LocalDate,
+    val numberOfInstallments: Int,
+    val customerId: Long
+) {
+    fun toEntity(): Credit = Credit(
+        creditValue = this.creditValue,
+        dayFirstInstallment = this.dayFirstOfInstallment,
+        numberOfInstallment = this.numberOfInstallments,
+        customer = Customer(id = this.customerId)
+    )
+
+
+}
