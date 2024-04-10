ed88de18fd79eb468ccc845405c1b0782952d357
dccouto <die_go0107@yahoo.com.br>
2024-04-09 22:37:23 -0300

    criando save no controller credit

--- src/main/kotlin/com/coutech/financeirosystem/controllers/CreditController.kt
+++ src/main/kotlin/com/coutech/financeirosystem/controllers/CreditController.kt
@@ -1,18 +1,22 @@
 package com.coutech.financeirosystem.controllers
 
+import com.coutech.financeirosystem.dtos.CreditDto
 import com.coutech.financeirosystem.entities.Credit
 import com.coutech.financeirosystem.service.impl.CreditService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(name = "credit")
-class CreditController(creditService: CreditService) {
+class CreditController(private val creditService: CreditService) {
 
-    fun createCredit(): ResponseEntity<Credit> {
-        TODO("Realizar o save")
-        return ResponseEntity.status(HttpStatus.OK).build()
+    @PostMapping
+    fun createCredit(@RequestBody creditDto: CreditDto): ResponseEntity<Credit> {
+        val creditEntity: Credit = creditDto.toEntity();
+        return ResponseEntity.status(HttpStatus.CREATED).body(creditService.save(creditEntity))
     }
 }
