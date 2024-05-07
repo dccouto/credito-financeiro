e98386460e1b1ff85f15b3436cd3643192ad87e7
dccouto <die_go0107@yahoo.com.br>
2024-05-06 23:10:48 -0300

    validando DTO com o @Valid

--- src/main/kotlin/com/coutech/financeirosystem/dtos/CreditDto.kt
+++ src/main/kotlin/com/coutech/financeirosystem/dtos/CreditDto.kt
@@ -4,11 +4,19 @@ import com.coutech.financeirosystem.entities.Credit
 import com.coutech.financeirosystem.entities.Customer
 import java.math.BigDecimal
 import java.time.LocalDate
+import javax.validation.constraints.Future
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
 
 data class CreditDto(
+    @field:NotNull
     val creditValue: BigDecimal,
+    @field:Future
     val dayFirstOfInstallment: LocalDate,
+    @field:Positive
     val numberOfInstallments: Int,
+    @field:NotNull
     val customerId: Long
 ) {
     fun toEntity(): Credit = Credit(
--- src/main/kotlin/com/coutech/financeirosystem/dtos/CustomerDto.kt
+++ src/main/kotlin/com/coutech/financeirosystem/dtos/CustomerDto.kt
@@ -2,16 +2,27 @@ package com.coutech.financeirosystem.dtos
 
 import com.coutech.financeirosystem.entities.Address
 import com.coutech.financeirosystem.entities.Customer
+import org.hibernate.validator.constraints.br.CPF
 import java.math.BigDecimal
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
 
-data class CustomerDto (
+data class CustomerDto(
+    @field:NotBlank(message = "Campo obrigatório")
     val firstName: String,
+    @field:NotBlank(message = "Campo obrigatório")
     val lastName: String,
+    @field:CPF(message = "Campo obrigatório")
     val cpf: String,
+    @field:NotBlank(message = "Campo obrigatório")
     val income: BigDecimal,
+    @field:Email(message = "Campo obrigatório")
     val email: String,
+    @field:NotBlank(message = "Campo obrigatório")
     val password: String,
+    @field:NotBlank(message = "Campo obrigatório")
     val zipCode: String,
+    @field:NotBlank(message = "Campo obrigatório")
     val street: String
 ) {
     fun toEntity(): Customer = Customer(
